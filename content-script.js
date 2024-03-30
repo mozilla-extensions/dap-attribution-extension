491ff382b0e557b5e0d4296bd6f51290fac62ea7
Brendan Birdsong <bbirdsong@mozilla.com>
2024-03-30 13:45:53 -0500

    Reset records after sending

--- content-script.js
+++ content-script.js
@@ -1,27 +1,25 @@
 "use strict";
 
 if (!window.hasRunAttributionScript) {
-  window.addEventListener("fx-attribution", handleEvent, false);
+    window.addEventListener("fx-attribution", handleEvent, false);
 }
 window.hasRunAttributionScript = true;
 
 const actions = {
-  [MEASUREMENT_TYPES.source.id]: handleSourceEvent,
-  [MEASUREMENT_TYPES.target.id]: handleTargetEvent,
-}
+    [MEASUREMENT_TYPES.source.id]: handleSourceEvent,
+    [MEASUREMENT_TYPES.target.id]: handleTargetEvent,
+};
 
 async function handleEvent(e) {
-  if (!browser.extension.inIncognitoContext) {
-    const payload = JSON.parse(window.atob(e.detail));
-    const measurement = MEASUREMENTS[payload.measurement];
+    if (!browser.extension.inIncognitoContext) {
+        const payload = JSON.parse(window.atob(e.detail));
+        const measurement = MEASUREMENTS[payload.measurement];
 
-    if (measurement) {
-      await actions[measurement.type](payload);
+        if (measurement) {
+            await actions[measurement.type](payload);
+        }
     }
-  }
 
-  // Dispatch event so clients can perform actions after completion
-  window.dispatchEvent(
-    new CustomEvent("fx-attribution-complete")
-  );
+    // Dispatch event so clients can perform actions after completion
+    window.dispatchEvent(new CustomEvent("fx-attribution-complete"));
 }
---
+++ websites/util/createAds.js
@@ -0,0 +1,72 @@
+var tasks = [
+  "hYycR_j0VUxBzS2MWi_mIKbMsTX2ZaGaIVvr03zPTt8",
+  "v17q4Bdx5Dv56Dk1isHE6BqfN9R9zAUWYWiWMC3rYyQ"
+];
+var taskSize = 5;
+
+var placements = [1, 2];
+var advertisers = [1, 2];
+
+
+let advAds = {};
+advertisers.forEach((a) => {
+  advAds[a] = Array.from({length: taskSize}, (_, i) => (i + 1) + ((a * taskSize) - taskSize));
+});
+
+let ads = [];
+let i = 0;
+placements.forEach((plc) => {
+  let curAdvAdIMap = {};
+  advertisers.forEach((a) => {
+    curAdvAdIMap[a] = 0;
+  });
+  tasks.forEach((t) => {
+    for (let tI = 0; tI < taskSize; tI++) {
+      const curAdv = advertisers[i % advertisers.length];
+      const curAdvAds = advAds[curAdv];
+      const curAd = curAdvAds[curAdvAdIMap[curAdv]++];
+      const plcAd = getPlcAd(plc, curAdv, curAd, curAd, "", t, tI, curAd);
+      const attrObj = {
+        taskId: plcAd.taskId,
+        taskIndex: plcAd.taskIndex,
+        taskSize: plcAd.taskSize,
+        key: plcAd.conversionKey,
+      };
+      ads.push({
+        plc: {
+          id: plcAd.placementId
+        },
+        ad: {
+          id: plcAd.adId,
+          name: plcAd.adName,
+          url: plcAd.adUrl,
+        },
+        att: {
+          imp: btoa(
+            JSON.stringify({ measurement: "impression", ...attrObj })
+          ),
+          cl: btoa(JSON.stringify({ measurement: "click", ...attrObj })),
+        },
+        // debug: plcAd,
+      });
+      i++;
+    }
+  });
+});
+
+console.log(JSON.stringify(ads));
+
+function getPlcAd(plcI, advI, adI, pId, url, tId, tI, kI) {
+  return {
+    placementId: "plc" + plcI,
+    advertiserId: "adv" + advI,
+    adId: "ad" + adI,
+    advertiserName: "Advertiser " + advI,
+    adName: "Product " + pId,
+    adUrl: url,
+    taskId: tId,
+    taskIndex: tI,
+    taskSize: taskSize,
+    conversionKey: "key" + kI,
+  }
+}
